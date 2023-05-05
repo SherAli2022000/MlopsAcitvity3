ebe3b523806e8116cd6033f80e91bbcc6827398a
Ali-Shah2022000 <104692601+Ali-Shah2022000@users.noreply.github.com>
2023-05-05 20:26:59 +0500

    Add DVC pipeline

--- train.py
+++ train.py
@@ -1,20 +1,30 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib
- 
+import json
 
-df = pd.read_csv('creditcard.csv')
+# Load data and select first 2000 rows
+df = pd.read_csv('creditcard.csv', nrows=2000)
+
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(df.drop('Class', axis=1), df['Class'], test_size=0.2, random_state=42)
 
+# Train a random forest classifier
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
 
-
+# Make predictions on the test set
 y_pred = rf.predict(X_test)
+
+# Calculate evaluation metrics
 accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
+precision = precision_score(y_test, y_pred)
+# Save metrics to a JSON file
+metrics = {'accuracy': accuracy, 'precision': precision}
+with open('metrics.json', 'w') as f:
+    json.dump(metrics, f)
 
-
+# Save model to a joblib file
 joblib.dump(rf, 'model.joblib')
